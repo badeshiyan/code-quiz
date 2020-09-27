eb94febfc65ac03ed35d2e26359a63784f0d3c84
Adebambo Adeshiyan <badeshiyan@gmail.com>
2020-09-27 18:21:07 -0400

    created quizquestions js file and added questions and answers per hw demo gif

---
+++ quizquestions.js
@@ -0,0 +1,36 @@
+// code quiz questions
+var quizquestions = [
+  {
+    question: "Commonly used data types DO NOT include:",
+    options: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts",
+  },
+  {
+    question:
+      "The condition in an if / else statement is enclosed within _______.",
+    options: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses",
+  },
+  {
+    question: "Arrays in Javascript can be used to store ________.",
+    options: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above",
+    ],
+    answer: "all of the above",
+  },
+  {
+    question:
+      "String values must be enclosed within ________ when being assigned to variables.",
+    options: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "quotes",
+  },
+  {
+    question:
+      "A very useful tool for used during development and debugging for printing content to the debugger is.",
+    options: ["Javascript", "terminal / bash", "for loops", "console log"],
+    answer: "console log",
+  },
+];
