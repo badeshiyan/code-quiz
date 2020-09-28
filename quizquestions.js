7721f5b3350333c18f65cfc2d6f50dea7b6d7de8
Adebambo Adeshiyan <badeshiyan@gmail.com>
2020-09-27 23:53:23 -0400

    made significant updates to js and html files

--- quizquestions.js
+++ quizquestions.js
@@ -1,4 +1,6 @@
 // code quiz questions
+console.log("Hello World");
+
 var quizquestions = [
   {
     question: "Commonly used data types DO NOT include:",
@@ -7,7 +9,7 @@ var quizquestions = [
   },
   {
     question:
-      "The condition in an if / else statement is enclosed within _______.",
+      "The condition of an if / else statement is enclosed within _______.",
     options: ["quotes", "curly brackets", "parentheses", "square brackets"],
     answer: "parentheses",
   },
@@ -34,3 +36,38 @@ var quizquestions = [
     answer: "console log",
   },
 ];
+
+var timer = document.querySelector(".time");
+var question = document.querySelector(".question");
+var btn1 = document.querySelector(".btn1");
+var btn2 = document.querySelector(".btn2");
+var btn3 = document.querySelector(".btn3");
+var btn4 = document.querySelector(".btn4");
+var secondsLeft = 75;
+
+function setTime() {
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    timer.textContent = "Time: " + secondsLeft;
+    if (secondsLeft === 0) {
+      clearInterval(timerInterval);
+      //   timer.textContent = " ";
+      alert("Your time is up!");
+    }
+  }, 1000);
+}
+function myTest() {
+  setTime();
+  for (var i = 0; i < quizquestions.length; i++) {
+    question.textContent = quizquestions[i].question;
+    btn1.textContent = quizquestions[i].options[0];
+    btn2.textContent = quizquestions[i].options[1];
+    btn3.textContent = quizquestions[i].options[2];
+    btn4.textContent = quizquestions[i].options[3];
+  }
+}
+myTest();
+btn1.addEventListener("click", null);
+btn2.addEventListener("click", null);
+btn3.addEventListener("click", null);
+btn4.addEventListener("click", null);
